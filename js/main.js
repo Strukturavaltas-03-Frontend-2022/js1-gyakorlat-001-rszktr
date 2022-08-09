19e12041344a784b705f49e738a237fa93278a45
rszktr <reszketo.robert@gmail.com>
2022-08-09 22:17:11 +0200

    megoldás (?)

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let buttonCollection = document.querySelectorAll('.button');
+
+let handleClick = () => {
+
+for (let i = 0; i < buttonCollection.length; i++) {
+    buttonCollection[i].addEventListener('click', function sayName() {return console.log(this.innerHTML)});
+};
+}
---
+++ js/person.js
@@ -0,0 +1,4 @@
+let personDataLog = (
+    firstname = 'John',
+    lastname = 'Doe',
+    age = 33 ) => { return console.log("My name is " + firstname + " " + lastname + ". "  + "I'm " + age + " old.");}
