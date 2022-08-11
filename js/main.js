e8532ddd8d19404b479e693d40acb75b8b943632
Joe <reszketo.robert@gmail.com>
2022-08-11 17:09:56 +0200

    v1.1 - test environment error fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-let buttonCollection = document.querySelectorAll('.button');
 
 let handleClick = () => {
-for (let i = 0; i < buttonCollection.length; i++) {
+    let buttonCollection = document.querySelectorAll('.button');
+    for (let i = 0; i < buttonCollection.length; i++) {
     buttonCollection[i].addEventListener('click', function sayName() {return console.log(this.innerHTML)});
 };
 }
